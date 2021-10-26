1986d00b8d8115892383dc5aac01bb527dbd59f3
Menezess42 <arielmenezess@hotmail.com>
2021-10-26 14:43:47 -0300

    i finished the docstrings

--- kruskalArielMenezess2.py
+++ kruskalArielMenezess2.py
@@ -28,13 +28,14 @@ for i in range(nA):
         v1 ([int]): [is the vertice one]
         v2 ([int]): [is the vertice two]
         c  ([int]): [is the cost of the edge v1,v2]
+        
     """
     aux = []
-    v1,v2,cust = input().split()
-    cust = int(cust)
+    v1,v2,c = input().split()
+    cust = int(c)
     v1 = int(v1)
     v2 = int(v2)
-    aux.append(cust)
+    aux.append(c)
     aux.append(v1)
     aux.append(v2)
     ord_per_cost(orded_edge_vector,aux)
@@ -61,22 +62,29 @@ cont = 0
 #cont is the amount of interactions of the list of vectors until they became one single tree
 cost = 0
 #cost is the sum of all the cost of the vertices with the algorithm pass to create the final tree
-vPé = 0
+
 while cont < nV-1:
-    c,v1,v2  = arestas_ord_valor.pop(vPé)
-    if componenetes[v1] != componenetes[v2]:
+    """[in this looping, the cont just increase if v1 and v2 don't belong to the same tree and stop when cont is nV-1,
+        this is the main part where the krukal algorithm logic real works]
+       
+       c ([int]): [receive the cost coming from the orded_edge_vector]
+       v1 ([int]): [receive the vertice one coming from the orded_edge_vector] 
+       v2 ([int]): [receive the vertice two coming from the orded_edge_vector]
+    """
+    c,v1,v2  = orded_edge_vector.pop(0)
+    if components[v1] != components[v2]:
         cost = cost + c
-        comp1 = componenetes[v1]
-        comp2 = componenetes[v2]
+        comp1 = components[v1]
+        comp2 = components[v2]
         if comp2 <= comp1:
             comp1, comp2 = comp2, comp1
         for i in vertice[comp2]:
-            componenetes[i] = comp1
+            components[i] = comp1
         vertice[comp1].extend(vertice[comp2])
         vertice[comp2] = []
         cont += 1
         print(vertice)
-        print(componenetes)
+        print(components)
         
 print(f"custo={cost}")        
     
